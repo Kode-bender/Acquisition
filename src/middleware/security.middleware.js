ee1862874730a716e07313557e2a62ae60ecda6d
steve <ourusteve13@gmail.com>
2025-09-16 03:15:19 +0300

    Secure our API w/ arcjet

---
+++ src/middleware/security.middleware.js
@@ -0,0 +1,86 @@
+import aj from '#config/arcjet.js';
+import logger from '#config/logger.js';
+import { slidingWindow } from '@arcjet/node';
+
+export const securityMiddleware = async (req, res, next) => {
+  try {
+    const role = req.user?.role || 'guest';
+
+    let limit;
+    let message;
+
+    switch (role) {
+      case 'admin':
+        limit = 20;
+        message = 'Admin request limit exceeded. Slow down';
+        break;
+      case 'user':
+        limit = 10;
+        message = 'User request limit exceeded. Slow down';
+        break;
+      case 'guest':
+        limit = 5;
+        message = 'Guest request limit exceeded. Slow down';
+        break;
+    }
+
+    const client = aj.withRule(
+      slidingWindow({
+        mode: 'LIVE',
+        interval: '1m',
+        max: limit,
+        name: `${role}-rate-limit`,
+      })
+    );
+
+    const decision = await client.protect(req);
+
+    if (decision.isDenied && decision.reason.isBot()) {
+      logger.warn('Bot request blocked', {
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        path: req.path,
+      });
+
+      return res.status(403).json({
+        error: 'Forbiden',
+        message: 'Automated request not allowed',
+      });
+    }
+
+    if (decision.isDenied && decision.reason.isShield()) {
+      logger.warn('Shield blocked request', {
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        path: req.path,
+        method: req.method,
+      });
+
+      return res.status(403).json({
+        error: 'Forbiden',
+        message: 'Request blocked by security policy',
+      });
+    }
+
+    if (decision.isDenied && decision.reason.isRateLimit()) {
+      logger.warn('Rate limit exceeded', {
+        ip: req.ip,
+        userAgent: req.get('User-Agent'),
+        path: req.path,
+      });
+
+      return res.status(403).json({
+        error: 'Forbiden',
+        message,
+      });
+    }
+
+    next();
+  } catch (e) {
+    console.error('Arcjet middleware error: ', e);
+    res.status(500).json({
+      error: 'Internal server error',
+      message: 'Something went wrong in security middleware',
+    });
+  }
+};
